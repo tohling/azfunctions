52f465ddae609881cca515165ec558f1c988b5e1
lingtoh <lingtoh@gmail.com>
2016-12-02 13:43:10 -0800

    Adding Functions to start, stop and get VM.

---
+++ GetVm/run.ps1
@@ -0,0 +1,21 @@
+$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+
+# Set Service Principal credentials
+# SP_PASSWORD, SP_USERNAME, TENANTID are app settings
+$secpasswd = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force;
+$mycreds = New-Object System.Management.Automation.PSCredential ($env:SP_USERNAME, $secpasswd)
+Add-AzureRmAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $mycreds;
+$context = Get-AzureRmContext;
+Set-AzureRmContext -Context $context;
+
+# Get VM
+Get-AzureRmVM -ResourceGroupName $requestBody.resourcegroup -Name $requestBody.vmname -Status | Out-String
+
+<#
+Sample POST JSON body:
+
+{
+   "vmname": "vmtest98052",
+   "resourcegroup": "vmtestResourceGroup"
+}
+#>
