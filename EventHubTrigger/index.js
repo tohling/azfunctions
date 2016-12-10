4ba692819ba96d966d5be24af8356b182452db3c
lingtoh <lingtoh@gmail.com>
2016-12-09 20:06:44 -0800

    Fixed config and code to use arrays.

--- EventHubTrigger/index.js
+++ EventHubTrigger/index.js
@@ -1,4 +1,12 @@
+var util = require('util');
+
 module.exports = function (context, myEventHubTrigger) {
-    context.log('JavaScript eventhub trigger function processed work item', myEventHubTrigger);
+    context.log(util.format("JavaScript eventhub trigger function processed %d events", myEventHubTrigger.length));
+
+    for (i = 0; i < myEventHubTrigger.length; i++)
+    {
+        context.log(myEventHubTrigger[i]);
+    }
+
     context.done();
-};
+}
